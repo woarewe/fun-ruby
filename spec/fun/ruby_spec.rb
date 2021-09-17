3c2aa64ebcdc86d889388383e944bfd71f31d4a6
woarewe <rostislav.zhuravsky@gmail.com>
2021-09-17 04:35:58 +0300

    Init commit

--- spec/fun/ruby_spec.rb
+++ spec/fun/ruby_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Fun::Ruby do
   it "has a version number" do
     expect(Fun::Ruby::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
