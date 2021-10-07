474b1732e6512178ca56a2bacf880361be802b0a
woarewe <rostislav.zhuravsky@gmail.com>
2021-10-08 00:52:00 +0300

    Add basic thread-safe container

---
+++ lib/fun_ruby/container.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module FunRuby
+  # @private
+  class Container
+    # @private
+    def initialize
+      @storage = {}
+      @mutex = Mutex.new
+    end
+
+    # @private
+    def define(full_key, function)
+      full_key = full_key.to_s
+
+      mutex.synchronize do
+        raise KeyError, "#{full_key.inspect} is already defined" if storage.key?(full_key)
+        raise TypeError, "should respond to #call" unless function.respond_to?(:call)
+
+        storage[full_key] = function
+      end
+    end
+
+    # @private
+    def fetch(full_key)
+      storage.fetch(full_key.to_s)
+    end
+
+    private
+
+    attr_reader :storage, :mutex
+  end
+
+  private_constant :Container
+end
---
+++ spec/fun_ruby/container_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require_lib "fun_ruby/container"
+
+describe FunRuby.const_get(:Container) do
+  describe "#define" do
+    it "defines a new function" do
+      container = described_class.new
+      function = ->(x, y) { x + y }
+      key = "sum"
+
+      container.define(key, function)
+
+      expect(container.fetch(key)).to equal(function)
+    end
+
+    it "raises error if key is already defined" do
+      container = described_class.new
+      function = ->(x, y) { x + y }
+      key = "sum"
+      container.define(key, function)
+
+      expect { container.define(key, function) }.to raise_error(KeyError)
+    end
+
+    it "raises error if function is not callable" do
+      container = described_class.new
+      function = Object.new
+      key = "sum"
+
+      expect { container.define(key, function) }.to raise_error(TypeError)
+    end
+  end
+
+  describe "#fetch" do
+    it "returns defined function" do
+      container = described_class.new
+      function = ->(x, y) { x + y }
+      key = "sum"
+
+      container.define(key, function)
+
+      expect(container.fetch(key)).to equal(function)
+    end
+
+    it "raises error if no registered key" do
+      container = described_class.new
+      key = "sum"
+
+      expect { container.fetch(key) }.to raise_error(KeyError)
+    end
+  end
+end
