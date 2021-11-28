c928f8e0bf4930e5e0826ac4a6a1163a78ac027b
woarewe <rostislav.zhuravsky@gmail.com>
2021-11-28 17:05:38 +0300

    Add Hash.put/3

--- lib/fun_ruby/hash.rb
+++ lib/fun_ruby/hash.rb
@@ -13,7 +13,7 @@ module FunRuby
     # @since 0.1.0
     #
     # @param key [#hash, #eql?]
-    # @param hash [#to_hash]
+    # @param hash [#to_h]
     #
     # @return [Object]
     #
@@ -54,7 +54,7 @@ module FunRuby
     # @since 0.1.0
     #
     # @param key [#hash, #eql?]
-    # @param hash [#to_hash]
+    # @param hash [#to_h]
     #
     # @return [Object]
     #
@@ -92,10 +92,9 @@ module FunRuby
     #
     # @param key [#hash, #eql?]
     # @param fallback [#to_call]
-    # @param hash [#to_hash]
+    # @param hash [#to_h]
     #
     # @return [Object]
-    #
     #
     # @example Base
     #
@@ -118,11 +117,50 @@ module FunRuby
     #   curried = F::Hash.fetch_with(F._, F._, F._)
     #   curried.(:age).(->(_) { :fallback }).(hash) # => 20
     #
-    #   curried = F::Hash.fetch_with(F._,->(_) { :fallback }), F._)
+    #   curried = F::Hash.fetch_with(F._, ->(_) { :fallback }, F._)
     #   curried.(:age).(hash) # => 20
     #   curried.(:email).(hash) # => :fallback
     def fetch_with(key = F._, fallback = F._, hash = F._)
       curry_implementation(:fetch_with, key, fallback, hash)
+    end
+
+    # Puts a value by a given key.
+    # If the key is already taken the value will be replaced.
+    #
+    # @since 0.1.0
+    #
+    # @param key [#hash, #eql?]
+    # @param value [Object]
+    # @param hash [#to_h]
+    #
+    # @return [Hash]
+    #
+    # @example Base
+    #
+    #   hash = { name: "John" }
+    #   F::Hash.put(:age, 20, hash) # => { name: "John", age: 20 }
+    #   F::Hash.put(:name, "Peter", hash) # => { name: "Peter" }
+    #
+    # @example Curried
+    #
+    #   hash = { name: "John" }
+    #   curried = F::Hash.put
+    #   curried.(:age).(20).(hash) # => { name: "John", age: 20 }
+    #   curried.(:name).("Peter").(hash) # => { name: "Peter" }
+    #
+    # @example Curried with placeholders
+    #
+    #   hash = { name: "John" }
+    #   curried = F::Hash.put(F._, 20, hash)
+    #   curried.(:age) # => { name: "John", age: 20 }
+    #
+    #   curried = F::Hash.put(F._, 20, F._)
+    #   curried.(:age).(hash) # => { name: "John", age: 20 }
+    #
+    #   curried = F::Hash.put(:age, F._, hash)
+    #   curried.(20) # => { name: "John", age: 20 }
+    def put(key = F._, value = F._, hash = F._)
+      curry_implementation(:put, key, value, hash)
     end
 
     private
@@ -137,6 +175,10 @@ module FunRuby
 
     def _fetch_with(key, fallback, hash)
       _hash(hash).fetch(key, &fallback)
+    end
+
+    def _put(key, value, hash)
+      _hash(hash).merge(key => value)
     end
 
     def _hash(hash)
