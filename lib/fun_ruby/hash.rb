b75a22eb626ff8593654d08f60a7f8cc2529fb19
woarewe <rostislav.zhuravsky@gmail.com>
2021-11-28 15:57:54 +0300

    Add Hash.fetch_with

--- lib/fun_ruby/hash.rb
+++ lib/fun_ruby/hash.rb
@@ -75,13 +75,6 @@ module FunRuby
     #   curried.(:name).(hash) # => 'John'
     #   curried.(:email).(hash) # => raise KeyError, "key not found: :email"
     #
-    # @example Curried
-    #
-    #   hash = { age: 20, name: 'John' }
-    #   curried = F::Hash.get
-    #   curried.(:age, hash) # => 20
-    #   curried.(:name).(hash) # => 'John'
-    #
     # @example Curried with placeholders
     #
     #   hash = { age: 20, name: 'John' }
@@ -92,6 +85,46 @@ module FunRuby
       curry_implementation(:fetch, key, hash)
     end
 
+    # Returns a value stored by a given key.
+    # If there is no given key it calls a given fallback.
+    #
+    # @since 0.1.0
+    #
+    # @param key [#hash, #eql?]
+    # @param fallback [#to_call]
+    # @param hash [#to_hash]
+    #
+    # @return [Object]
+    #
+    #
+    # @example Base
+    #
+    #   hash = { age: 20, name: 'John' }
+    #   F::Hash.fetch_with(:age, ->(_) { :fallback }, hash) # => 20
+    #   F::Hash.fetch_with(:name, ->(_) { :fallback }, hash) # => 'John'
+    #   F::Hash.fetch_with(:email, ->(_) { :fallback }, hash) # => :fallback
+    #
+    # @example Curried
+    #
+    #   hash = { age: 20, name: 'John' }
+    #   curried = F::Hash.fetch_with
+    #   curried.(:age, ->(_) { :fallback }, hash) # => 20
+    #   curried.(:name).(->(_) { :fallback }).(hash) # => 'John'
+    #   curried.(:email).(->(_) { :fallback }).(hash) # => :fallback
+    #
+    # @example Curried with placeholders
+    #
+    #   hash = { age: 20, name: 'John' }
+    #   curried = F::Hash.fetch_with(F._, F._, F._)
+    #   curried.(:age).(->(_) { :fallback }).(hash) # => 20
+    #
+    #   curried = F::Hash.fetch_with(F._,->(_) { :fallback }), F._)
+    #   curried.(:age).(hash) # => 20
+    #   curried.(:email).(hash) # => :fallback
+    def fetch_with(key = F._, fallback = F._, hash = F._)
+      curry_implementation(:fetch_with, key, fallback, hash)
+    end
+
     private
 
     def _get(key, hash)
@@ -100,6 +133,10 @@ module FunRuby
 
     def _fetch(key, hash)
       _hash(hash).fetch(key)
+    end
+
+    def _fetch_with(key, fallback, hash)
+      _hash(hash).fetch(key, &fallback)
     end
 
     def _hash(hash)
