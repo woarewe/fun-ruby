4674041198d250a112cde88333bced88aaf5823f
woarewe <rostislav.zhuravsky@gmail.com>
2022-04-28 03:22:22 +0300

    Add String.concat

--- lib/fun_ruby/string.rb
+++ lib/fun_ruby/string.rb
@@ -32,10 +32,37 @@ module FunRuby
       curry_implementation(:split, splitter, string)
     end
 
+    # Concats a string by a passed delimiter
+    #
+    # @since 0.1.0
+    #
+    # @param first [::String]
+    # @param second [::String]
+    #
+    # @return [::String]
+    #
+    # @example Basics
+    #   F::String.concat("123", "456") #=> "123456"
+    #
+    # @example Curried
+    #   curried = F::String.concat
+    #   curried.("123").("456") #=> "123456"
+    #
+    # @example Curried with placeholder
+    #   curried = F::String.concat(F._, "456")
+    #   curried.("123") #=> "123456"
+    def concat(first = F._, second = F._)
+      curry_implementation(:concat, first, second)
+    end
+
     private
 
     def _split(splitter, string)
       _string(string).split(splitter)
+    end
+
+    def _concat(first, second)
+      _string(first) + _string(second)
     end
 
     def _string(string)
