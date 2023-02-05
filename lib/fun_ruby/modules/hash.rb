c78dfb29bb445088f6b0cf9ecf3e0d798f844a52
woarewe <rostislav.zhuravsky@gmail.com>
2023-02-05 15:28:37 +0100

    Move fetch with

--- lib/fun_ruby/modules/hash.rb
+++ lib/fun_ruby/modules/hash.rb
@@ -9,42 +9,6 @@ module FunRuby
 
       extend self
 
-      # Returns a value stored by a given key.
-      # If there is no given key it calls a given fallback.
-      #
-      # @since 0.1.0
-      #
-      # @param key [#hash, #eql?]
-      # @param fallback [#to_call]
-      # @param hash [#to_h]
-      #
-      # @return [Object]
-      #
-      # @example Base
-      #   hash = { age: 20, name: 'John' }
-      #   F::Modules::Hash.fetch_with(:age, ->(_) { :fallback }, hash) # => 20
-      #   F::Modules::Hash.fetch_with(:name, ->(_) { :fallback }, hash) # => 'John'
-      #   F::Modules::Hash.fetch_with(:email, ->(_) { :fallback }, hash) # => :fallback
-      #
-      # @example Curried
-      #   hash = { age: 20, name: 'John' }
-      #   curried = F::Modules::Hash.fetch_with
-      #   curried.(:age, ->(_) { :fallback }, hash) # => 20
-      #   curried.(:name).(->(_) { :fallback }).(hash) # => 'John'
-      #   curried.(:email).(->(_) { :fallback }).(hash) # => :fallback
-      #
-      # @example Curried with placeholders
-      #   hash = { age: 20, name: 'John' }
-      #   curried = F::Modules::Hash.fetch_with(F._, F._, F._)
-      #   curried.(:age).(->(_) { :fallback }).(hash) # => 20
-      #
-      #   curried = F::Modules::Hash.fetch_with(F._, ->(_) { :fallback }, F._)
-      #   curried.(:age).(hash) # => 20
-      #   curried.(:email).(hash) # => :fallback
-      def fetch_with(key = F._, fallback = F._, hash = F._)
-        curry_implementation(:fetch_with, key, fallback, hash)
-      end
-
       # Puts a value by a given key.
       # If the key is already taken the current value will be replaced.
       #
@@ -492,3 +456,4 @@ end
 
 require_relative "hash/get"
 require_relative "hash/fetch"
+require_relative "hash/fetch_with"
