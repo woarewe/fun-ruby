d94245cbd7c63d12abccee620604de2f50e5e9a9
kirillpt <kirp@dismail.de>
2023-02-15 23:55:36 +0300

    Modules::String .strip add some examples with special chars

--- lib/fun_ruby/modules/string/strip.rb
+++ lib/fun_ruby/modules/string/strip.rb
@@ -14,14 +14,18 @@ module FunRuby
       #
       # @example Basics
       #   F::Modules::String.strip("  hello  ") #=> "hello"
+      #   F::Modules::String.strip("\n\r  hello  \n") #=> "hello"
+      #   F::Modules::String.strip("\n\r  hello  \n\r") #=> "hello"
       #
       # @example Curried
       #   curried = F::Modules::String.strip
       #   curried.("  hello  ") #=> "hello"
+      #   curried.("\n\r  hello  \n\r") #=> "hello"
       #
       # @example Curried with placeholder
       #   curried = F::Modules::String.strip(F._)
       #   curried.("  hello  ") #=> "hello"
+      #   curried.("\n\r  hello  \n\r") #=> "hello"
       def strip(string = F._)
         curry_implementation(:strip, string)
       end
